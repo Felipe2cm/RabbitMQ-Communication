b9af5522fd5fb0fc566245d5c53294793fd55e2d
Felipe Maia <felipe.cmaia@hotmail.com>
2022-02-19 11:12:50 -0300

    First commit

---
+++ RabbitMQCommunication/Consumers/ProcessMessageConsumer.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using RabbitMQCommunication.InputModel;
+using System.Text;
+using RabbitMQCommunication.Options;
+using RabbitMQCommunication.Services;
+
+namespace RabbitMQCommunication.Consumers
+{
+    public class ProcessMessageConsumer : BackgroundService
+    {
+        private readonly RabbitMqConfiguration _configuration;
+        private readonly IConnection _connection;
+        private readonly IModel _channel;
+        private readonly IServiceProvider _provider;
+
+        public ProcessMessageConsumer(IOptions<RabbitMqConfiguration> options, IServiceProvider provider)
+        {
+            _configuration = options.Value;
+            _provider = provider;
+
+            var factory = new ConnectionFactory
+            {
+                HostName = _configuration.Host,
+            };
+
+            _connection = factory.CreateConnection();
+            _channel = _connection.CreateModel();
+            _channel.QueueDeclare(
+                        queue: _configuration.Queue,
+                        durable: false,
+                        autoDelete: false,
+                        exclusive: false,
+                        arguments: null
+                        );
+            
+        }
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            var consumer = new EventingBasicConsumer(_channel);
+
+            consumer.Received += (sender, args) =>
+            {
+                var contentArray = args.Body.ToArray();
+                var contentString = Encoding.UTF8.GetString(contentArray);
+                var message = JsonConvert.DeserializeObject<MessageInputModel>(contentString);
+
+                NotifyUser(message);
+
+                _channel.BasicAck(args.DeliveryTag, false);
+            };
+
+            _channel.BasicConsume(_configuration.Queue, false, consumer);
+
+            return Task.CompletedTask;
+        }
+
+        public void NotifyUser(MessageInputModel message)
+        {
+            using (var scope = _provider.CreateScope())
+            {
+                var notificationService = scope.ServiceProvider.GetRequiredService<INotificationService>();
+
+                notificationService.NotifyUser(message.FromId, message.ToId, message.Content);
+            }
+        }
+    }
+}
---
+++ RabbitMQCommunication/Controllers/MessageController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQCommunication.InputModel;
+using RabbitMQCommunication.Options;
+using System.Text;
+
+namespace RabbitMQCommunication.Controllers
+{
+    [ApiController]
+    public class MessageController: Controller
+    {
+        private readonly ConnectionFactory _factory;
+        private readonly RabbitMqConfiguration _rabbitMqConfiguration;
+        private readonly string QUEUE_NAME;        
+
+        public MessageController(IOptions<RabbitMqConfiguration> options)
+        {
+            _rabbitMqConfiguration = options.Value;
+
+
+            _factory = new ConnectionFactory
+            { 
+                HostName = _rabbitMqConfiguration.Host,
+            };
+
+            QUEUE_NAME = _rabbitMqConfiguration.Host;
+
+        }
+
+        [HttpPost("PostMessage")]
+        public IActionResult PostMessage(MessageInputModel message)
+        {
+            using (var connection = _factory.CreateConnection())
+            {
+                using(var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(
+                        queue: QUEUE_NAME,
+                        durable: false,
+                        autoDelete: false,
+                        exclusive: false,
+                        arguments: null
+                        );
+
+                    var stringfieldMessage = JsonConvert.SerializeObject(message);
+                    var bytesMessage = Encoding.UTF8.GetBytes(stringfieldMessage);
+
+                    channel.BasicPublish(
+                        exchange: "",
+                        routingKey: QUEUE_NAME,
+                        basicProperties: null,
+                        body: bytesMessage);                    
+                }
+            }
+
+            return Accepted();
+        }
+
+
+    }
+}
